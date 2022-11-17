6e3918b6d5f2604cf1f40cbae8de7963f0037826
gabri-rabaglio <115536884+gabri-rabaglio@users.noreply.github.com>
2022-11-17 18:32:10 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,18 @@
 #include <iostream>
+#include <math.h>
 using namespace std;
-
 int main() {
+float a, b, x, y;   
+  cin>>a>>b>>x>>y;
+   if(x<0 and y>0+){
+   z = a*x – b*y;
+      cout<<z<<endl;
+   }else if(x>=o and y<=0){
+   z=a*pow(x,2)-b*y;
+       cout<<z<<endl;
+   }else{
+   z = a*x + b*pow(y,2);
+      cout<<z<<endl;
+   }
    return 0;
 }
